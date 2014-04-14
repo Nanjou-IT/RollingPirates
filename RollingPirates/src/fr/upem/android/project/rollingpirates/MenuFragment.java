1d2d631d4af542fc34e905da789367ce3189368e
hdahmani <hdahmani@pccop1b014-09.univ-mlv.fr>
2014-04-14 17:06:46 +0200

    Second commit!!!!

---
+++ RollingPirates/src/fr/upem/android/project/rollingpirates/MenuFragment.java
@@ -0,0 +1,16 @@
+package fr.upem.android.project.rollingpirates;
+
+import android.app.Fragment;
+
+public class MenuFragment extends Fragment{
+	public MenuFragment() {
+	
+	}
+	public static MenuFragment createMainMenu() {
+		MenuFragment mf =  new MenuFragment();
+		
+		return mf;
+	}
+	
+	
+}
