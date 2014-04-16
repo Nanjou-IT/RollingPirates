1f3f4294450d38ca2af86488f5c36bd6aea24e8a
hdahmani <hdahmani@pccop1b014-09.univ-mlv.fr>
2014-04-16 14:35:56 +0200

    commit assets

--- RollingPirates/src/fr/upem/android/project/rollingpirates/MenuFragment.java
+++ RollingPirates/src/fr/upem/android/project/rollingpirates/MenuFragment.java
@@ -1,17 +1,22 @@
 package fr.upem.android.project.rollingpirates;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MenuFragment extends Fragment{
 	public MenuFragment() {
 	
 	}
 	public static MenuFragment createMainMenu() {
-		return new MenuFragment(); //je laisse vide pour l'instant pour des raisons de compilation obscure
+		return new MenuFragment(); 
 	}
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,6 +25,16 @@ public class MenuFragment extends Fragment{
 		
 		super.onCreateView(inflater, container, savedInstanceState);
 		View v = inflater.inflate(R.layout.mainmenu_layout, container);
+		ViewGroup parentView = (ViewGroup) v.getParent();
+		
+		//int count = parentView.getChildCount();
+		
+//		Log.d("DEBUG",Integer.toString(count));
+		TextView tv = (TextView) v.findViewById(R.id.begin);
+		Context c = (Context) getActivity();
+		AssetManager am = c.getAssets();
+		Typeface mainMenu_tf = Typeface.createFromAsset(am, "fonts/oj.ttf");
+		tv.setTypeface(mainMenu_tf);
 		return v;
 	}
 
